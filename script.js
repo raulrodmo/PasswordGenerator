d360eabf6653e5721e52e59fbc367387b20c6182
Raul Rodrigues <raulrm.dev@gmail.com>
2022-09-22 19:29:47 -0300

    adjustmente js

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ function generatePassword(){
 }
 
 function copyPassword(){
-    alert("Senha copiada com sucesso")
-    navigator.clipboard.writeText(novaSenha)
+    navigator.clipboard.writeText(novaSenha);
+    alert("Senha copiada com sucesso");
 }
